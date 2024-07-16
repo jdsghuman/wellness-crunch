c908356d97c9f4317f31c6f5084929f59e37008a
JD Ghuman <jdsghuman@gmail.com>
2024-07-16 09:01:51 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@components/components/navbar";
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
